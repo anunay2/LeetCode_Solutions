9a43787dd435ceb3507a2a6303fa8266c61b69be
Anunay Kumar <35661357+anunay2@users.noreply.github.com>
2021-05-22 16:27:44 +0530

    Time: 8 ms (45.38%), Space: 5.8 MB (65.88%) - LeetHub

---
+++ arranging-coins/arranging-coins.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int MOD=10000007;
+    int arrangeCoins(int n) {
+        long long int i=1;
+      for(i=1;(i*(i+1))/2.0<=n;i++){
+          
+      }
+    return (i-1);
+    }
+};
